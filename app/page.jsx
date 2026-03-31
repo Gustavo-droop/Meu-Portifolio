d511d117b989d8d7cf081ef7391edf55b302d63b
Agendio. <139300553+Gustavo-droop@users.noreply.github.com>
2026-03-31 01:50:00 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import HeroSection      from "@/components/HeroSection";
 import ProblemSection   from "@/components/ProblemSection";
 import ServicesSection  from "@/components/ServicesSection";
 import PortfolioSection from "@/components/PortfolioSection";
-import SocialProof      from "@/components/SocialProof";
 import ProcessSection   from "@/components/ProcessSection";
+import SocialProof      from "@/components/SocialProof";
 import CTASection       from "@/components/CTASection";
 import Footer           from "@/components/Footer";
 
@@ -16,8 +16,8 @@ export default function Home() {
       <ProblemSection />
       <ServicesSection />
       <PortfolioSection />
-      <SocialProof />
       <ProcessSection />
+      <SocialProof />
       <CTASection />
       <Footer />
     </>
